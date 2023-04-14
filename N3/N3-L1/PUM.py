656975e4cd4bfb0e036a01ba613b292d79a894bf
PeterKlaus18 <p.pachon@uniandes.edu.co>
2023-04-13 19:31:40 -0500

    Update PUM.py

--- N3/N3-L1/PUM.py
+++ N3/N3-L1/PUM.py
@@ -5,24 +5,35 @@ Created on Wed Apr 12 17:09:09 2023
 @author: Nicolas
 """
 
-def PUM(n:int, x:int)->:
+def PUM(nj:int, o:int)->None:
     
+    x=o
+    k=0
     i=0
-    while i < 500 and x < 10:
-        
-        h=1
-        h+=1
-        if h== n+1:
-            h=1
-        
-        
-        u=1
-        
-        y= x*u
-        
-        u+=1
-        
-        
-        i+=1
-        print()
+    y=2
+    n=1
+    j=1
     
+    while i < 50 and o < 10:
+        
+        if j== nj+1:
+            j=1
+        
+            
+        if n == x or n == k:
+            
+            k= o * y
+            x*=k
+            
+            y+=1
+            
+            print(str(j)+" pum")
+        else:
+            print(j, n)
+            
+            
+        j+=1
+        i+=1
+        n+=1
+
+PUM(3, 5)
